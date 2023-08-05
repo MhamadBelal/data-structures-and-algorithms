a13e3858f4f51e63585a14d7395770df10aa82d0
Mhamad Belal Al Msalma <117995558+MhamadBelal@users.noreply.github.com>
2023-08-05 05:56:20 +0300

    tree-max challenge

---
+++ Tree-max/TreeMaxCode/BinaryTree.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeMaxCode
+{
+    public class BinaryTree
+    {
+        public Node Root;
+        public BinaryTree()
+        {
+            Root = null;
+        }
+
+
+        public int FindMaxValue()
+        {
+            if (Root == null)
+            {
+                throw new InvalidOperationException("Binary tree is empty.");
+            }
+
+            int maxValue = int.MinValue;
+
+            void DFS(Node node)
+            {
+                if (node == null)
+                {
+                    return;
+                }
+
+                // Update the maxValue if the current node's value is greater
+                if (node.Value > maxValue)
+                {
+                    maxValue = node.Value;
+                }
+
+                DFS(node.Left);
+                DFS(node.Right);
+            }
+
+            DFS(Root);
+
+            return maxValue;
+        }
+    }
+}
