d7e6e251dc15a02e51e826f04332a229b380ee83
Mhamad Belal Al Msalma <117995558+MhamadBelal@users.noreply.github.com>
2023-09-02 04:31:14 +0300

    Merge Sort

---
+++ MergeSort/MergeSortCode/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MergeSortCode
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 38, 27, 43, 3, 9, 82, 10 };
+
+            Console.WriteLine("Original Array:");
+            Console.WriteLine(string.Join(", ", arr));
+
+            MergeSort.Mergesort(arr);
+
+            Console.WriteLine("Sorted Array:");
+            Console.WriteLine(string.Join(", ", arr));
+        }
+    }
+}
